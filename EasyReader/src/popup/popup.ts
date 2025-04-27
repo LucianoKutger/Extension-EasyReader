c74bd16570b8e555341bf453553c2429315800d6
Luciano Kutger <Luciano.kutger@web.de>
2025-04-27 20:19:13 +0200

    create popup.ts

---
+++ EasyReader/src/popup/popup.ts
@@ -0,0 +1,11 @@
+const leichteSpracheButton = document.getElementById('leichteSprache')
+
+if(leichteSpracheButton){
+    leichteSpracheButton.addEventListener('click', () =>{
+    
+    })
+}else{
+    throw new Error("there is no Button with the id 'leichteSprache'")
+}
+
+
