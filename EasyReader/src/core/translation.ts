084b31ac8d7659ee1cd6d6dc12d2bc9fb5d43a13
Luciano Kutger <Luciano.kutger@web.de>
2025-04-27 15:02:09 +0200

    start implementing ai call

--- EasyReader/src/core/translation.ts
+++ EasyReader/src/core/translation.ts
@@ -1,21 +1,27 @@
 import * as localStorage from './local-storage.js'
 import * as hashing from './hashing.js'
 import * as supabase from './supabase.js'
+import * as openAi from './openAi.js'
 
 
 async function getTranslation(text: string, mode: string): Promise<string>{
-    const hashString = hashing.hashString(text)
-    const localStorageCheck = await localStorage.getTranslationFromStorage(hashString)
+    const hashString: string = hashing.hashString(text)
+    const localStorageCheck: string | null = await localStorage.getTranslationFromStorage(hashString)
     if(localStorageCheck !== null){
         return localStorageCheck
     }
 
-    const supabaseCheck = await supabase.checkForTranslationinSupabase(hashString, mode)
+    const supabaseCheck: string | null = await supabase.checkForTranslationinSupabase(hashString, mode)
     if(supabaseCheck !== null){
         return supabaseCheck
     }
-    //TODO: Implement call to Proxy for ai translation and return response
-    const aiResponse = "Insert AI logic"
-
+    //TODO: Implement error handeling(vorrübergehend)
+    const aiResponse: string = await openAi.getTranslationFromAi(text)
+    if(aiResponse === 'error'){
+        return 'Es ist ein Fehler aufgetreten versuche es nochmal'
+    }
+    
     return aiResponse
+    
+
 }
