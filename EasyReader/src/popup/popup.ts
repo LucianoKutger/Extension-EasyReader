bddf9f23cb654413e48ed9b066eb0c37ba9a0371
Luciano Kutger <Luciano.kutger@web.de>
2025-04-28 00:43:57 +0200

    fix popup

--- EasyReader/src/popup/popup.ts
+++ EasyReader/src/popup/popup.ts
@@ -2,7 +2,11 @@ const leichteSpracheButton = document.getElementById('leichteSprache')
 
 if(leichteSpracheButton){
     leichteSpracheButton.addEventListener('click', () =>{
-    
+        chrome.runtime.sendMessage({
+            action: 'wait for click', 
+            mode: "leicht"
+        })
+        alert("LeichteSprache")
     })
 }else{
     throw new Error("there is no Button with the id 'leichteSprache'")
