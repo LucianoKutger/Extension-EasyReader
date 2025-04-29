3f3608fabc7c86f7a8d1baa165ed542481303eb8
Luciano Kutger <Luciano.kutger@web.de>
2025-04-29 12:39:45 +0200

    add remove exports.js

---
+++ helper/remove-exports.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+const filesToFix = [
+    "EasyReader/dist/EasyReader/src/popup/popup.js",
+    "EasyReader/dist/EasyReader/sec/content/main.js"
+];
+
+filesToFix.forEach(file => {
+    if (fs.existsSync(file)) {
+        let content = fs.readFileSync(file, "utf-8");
+        content = content.replace(/export\s*\{\s*\};/g, ""); // export {}; raushauen
+        fs.writeFileSync(file, content, "utf-8");
+        console.log(`Fixed exports in ${file}`);
+    }
+});
