b51fb6763ff50d8c8677b64ab7bd1c8a7efd872b
Luciano Kutger <Luciano.kutger@web.de>
2025-04-29 15:13:44 +0200

    fix service worker

--- EasyReader/src/background/index.ts
+++ EasyReader/src/background/index.ts
@@ -1,17 +1,24 @@
 import { getTranslation } from "../core/translation.js"
-import { runtimeMessage, tabSendMessage } from "../types/messageType.js"
+import { runtimeMessage } from "../types/messageType.js"
 
+console.log("aktiv")
 
 chrome.runtime.onMessage.addListener((message: runtimeMessage, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void) => {
+    console.log(message)
     if (message.action === "wait for click") {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             if (tabs[0]?.id) {
                 chrome.tabs.sendMessage(tabs[0].id, {
                     action: "wait for click on text",
                     mode: message.mode
-                })
+                });
+
+
+                sendResponse({ status: "angekommen" })
             }
         })
+
+        return true
     }
 
     if (message.action === "clicked") {
--- EasyReader/src/popup/popup.ts
+++ EasyReader/src/popup/popup.ts
@@ -2,10 +2,20 @@ const leichteSpracheButton = document.getElementById('leichteSprache')
 
 if (leichteSpracheButton) {
     leichteSpracheButton.addEventListener('click', () => {
-        chrome.runtime.sendMessage({
-            action: 'wait for click',
-            mode: "leicht"
-        })
+        chrome.runtime.sendMessage(
+            {
+                action: 'wait for click',
+                mode: "leicht"
+            },
+            (response) => {
+                if (chrome.runtime.lastError) {
+                    console.error("Fehler:", chrome.runtime.lastError);
+                } else {
+                    console.log("Antwort vom Background:", response);
+                }
+            }
+        );
+
 
     })
 } else {
